e62ee0dd45a7fde44e8bd64543010e56e106950a
Candela <20242405@itla.edu.do>
2026-06-10 02:54:51 -0400

    Reorganize project structure into Source/Core folders

--- EVote360Pro.Core.Domain/Common/BasicEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EVote360Pro.Core.Domain.Common
-{
-    public class BasicEntity<TKey>
-    {
-        public required TKey Id { get; set; }
-        public bool Activo { get; set; }
-    }
-}
